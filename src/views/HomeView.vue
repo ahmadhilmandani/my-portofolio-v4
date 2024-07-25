d0523af895d17da09e71d99b943156e88ab92e7f
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-07-25 20:14:09 +0700

    UPDATE responsiveness on home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,29 @@
 <template>
   <div
-    class="w-full min-h-screen flex justify-center items-end bg-cust-black pt-14 pb-12 px-16 relative gap-28 text-cust-white">
+    class="w-full min-h-screen xl:flex justify-center items-end bg-cust-black pt-32 pb-12 px-8 xl:px-16 relative text-cust-white">
     <img :src="BlueDark" alt="" class="fixed top-0 left-0 z-[1]">
-    <div class="w-[50%]">
-      <img :src="MeColored" alt="" class="w-[340px] relative left-6 top-12 grayscale z-[2]">
-      <h1 class="text-cust-white font-bold text-6xl relative z-[2]">
+    <div class="max-w-[640px] w-full xl:mx-0 mx-auto">
+      <h1 class="xl:hidden text-center block text-cust-white font-bold text-6xl relative z-[2] leading-relaxed">
         Ahmad Hilman Dani
       </h1>
-      <h2 class="text-2xl mt-6 leading-[160%]">
+      <h2 class="xl:hidden text-center block text-xl mt-3 leading-[160%]">
+        Learn to mastering Frontend Web Dev and UI/X Design
+      </h2>
+
+      <img :src="MeColored" alt="" class="max-w-[340px] w-full relative left-0 xl:left-6 top-12 grayscale z-[2] xl:mx-0 mx-auto">
+      <h1 class="hidden xl:block text-cust-white font-bold text-6xl relative z-[2]">
+        Ahmad Hilman Dani
+      </h1>
+      <h2 class="hidden xl:block text-2xl mt-6 leading-[160%]">
         Learn to mastering Frontend Web Dev <br> and UI/X Design
       </h2>
     </div>
     <div class="flex-1">
-      <p class="w-[90%] text-[15px]">
+      <p class="w-[90%] text-[15px] xl:mx-0 mx-auto xl:text-left text-center">
         More than 6 months as a Frontend Web Developer (intern & part-time) and still going strong! I mostly work with Vue, but
         React is also my buddy!
       </p>
-      <RouterLink to="/experience-achievment" class="block w-fit mt-5">
+      <RouterLink to="/experience-achievment" class="block w-fit mt-5 mx-auto xl:mx-0 relative z-[2]">
         <ButtonComp styleProp="fill">
           See Detail <IconArrowNarrowRight class="w-5" />
         </ButtonComp>
