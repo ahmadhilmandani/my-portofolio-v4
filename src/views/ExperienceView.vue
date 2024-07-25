74124a603ef0cd60d422d76091774e3b466d8f69
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-07-25 20:19:39 +0700

    UPDATE responsiveness Experience View

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="w-full min-h-screen  pt-14 pb-12 px-16 relative text-cust-white">
+  <div class="w-full min-h-screen  pt-14 pb-12 px-8 xl:px-16 relative text-cust-white">
     <img :src="BlueDark" alt="" class="fixed top-0 left-0 z-[1]">
 
     <main class="mt-20">
-      <h1 class="text-cust-white font-bold text-4xl relative z-[2]">
+      <h1 class="text-cust-white font-bold text-4xl relative z-[2] leading-relaxed">
         Experiences & Achievment
       </h1>
       <p class="text-white text-xl mt-4 w-[80%] leading-relaxed mb-28">
@@ -12,11 +12,15 @@
 
 
       <div v-for="(valueOfExperienceData, index) in experienceData" :class="index === experienceData.length-1 ? 'pb-0' : 'pb-24'" class="flex gap-20 items-start pl-8 border-l border-cust-gray-lighter">
-        <div class="text-cust-gray-lighter">
+        <div class="hidden xl:block text-cust-gray-lighter">
           {{ valueOfExperienceData.year }} <br>
           {{ valueOfExperienceData.months }}
         </div>
-        <div class="w-[560px]">
+        <div class="max-w-[560px] w-full">
+          <div class="xl:hidden block text-cust-gray-lighter mb-2">
+          {{ valueOfExperienceData.year }} <br>
+          {{ valueOfExperienceData.months }}
+        </div>
           <h2 class="font-bold">{{ valueOfExperienceData.title }}</h2>
           <p class="text-sm mt-3 leading-[1.8]">
             {{ valueOfExperienceData.body }}
